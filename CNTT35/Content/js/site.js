ab69794699734ba058cc7febcc5a0e7a0d9bada0
LocBui <LocBui@DESKTOP-IJO1K2O>
2022-12-04 18:34:13 +0700

    select radio button Option 1 ,2

--- CNTT35/Content/js/site.js
+++ CNTT35/Content/js/site.js
@@ -98,3 +98,42 @@ function renderCity(data) {
 
 
 /*JS carousel  */
+
+function myFunction() {
+    var x = document.getElementById("option1");
+    var y = document.getElementById("option2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+
+}
+
+
+//function myFunction() {
+//    var x = document.getElementById("option1");
+
+//  if (x.style.display === "none") {
+//    x.style.display = "block";
+//  } else {
+//    x.style.display = "none";
+//    }
+
+//}
+//function myFunction2() {
+
+//    var y = document.getElementById("option2");
+//    if (y.style.display === "none") {
+//        y.style.display = "block";
+//    } else {
+//        y.style.display = "none";
+//    }
+
+//}
+
+$('input[type="checkbox"]').on('change', function () {
+    $('input[type="checkbox"]').not(this).prop('checked', false);
+});
