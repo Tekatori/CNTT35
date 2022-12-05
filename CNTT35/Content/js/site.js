8f979fd62183dfd13b05cfddd7668682827186c5
LocBui <LocBui@DESKTOP-IJO1K2O>
2022-12-05 18:35:08 +0700

    add file site js, css

--- CNTT35/Content/js/site.js
+++ CNTT35/Content/js/site.js
@@ -111,29 +111,59 @@ function myFunction() {
     }
 
 }
-
-
-//function myFunction() {
-//    var x = document.getElementById("option1");
-
-//  if (x.style.display === "none") {
-//    x.style.display = "block";
-//  } else {
-//    x.style.display = "none";
-//    }
-
-//}
-//function myFunction2() {
-
-//    var y = document.getElementById("option2");
-//    if (y.style.display === "none") {
-//        y.style.display = "block";
-//    } else {
-//        y.style.display = "none";
-//    }
-
-//}
-
+s
 $('input[type="checkbox"]').on('change', function () {
     $('input[type="checkbox"]').not(this).prop('checked', false);
 });
+
+"use strict";
+// Select all slides
+const slides = document.querySelectorAll(".slide");
+
+// loop through slides and set each slides translateX
+slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+// select next slide button
+const nextSlide = document.querySelector(".btn-next");
+
+// current slide counter
+let curSlide = 0;
+// maximum number of slides
+let maxSlide = slides.length - 1;
+
+// add event listener and navigation functionality
+nextSlide.addEventListener("click", function () {
+    // check if current slide is the last and reset current slide
+    if (curSlide === maxSlide) {
+        curSlide = 0;
+    } else {
+        curSlide++;
+    }
+
+    //   move slide by -100%
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
+});
+
+// select next slide button
+const prevSlide = document.querySelector(".btn-prev");
+
+// add event listener and navigation functionality
+prevSlide.addEventListener("click", function () {
+    // check if current slide is the first and reset current slide to last
+    if (curSlide === 0) {
+        curSlide = maxSlide;
+    } else {
+        curSlide--;
+    }
+
+    //   move slide by 100%
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
+});
+
+
