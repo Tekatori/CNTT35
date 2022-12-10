3949783c095fb7d7e2fd50c3ffc7cbd3b2f7ce22
LocBui <LocBui@DESKTOP-IJO1K2O>
2022-12-11 00:32:07 +0700

    change site css and js

--- CNTT35/Content/js/site.js
+++ CNTT35/Content/js/site.js
@@ -120,4 +120,31 @@ $('input[type="checkbox"]').on('change', function () {
 
 
 
+// Tab-Pane change function
+//function tabChange() {
+//    var tabs = $('.nav-tabs > li');
+//    var active = tabs.filter('.active');
+//    var next = active.next('li').length ? active.next('li').find('a') : tabs.filter(':first-child').find('a');
+//    next.tab('show');
+//}
+
+//$('.tab-pane').hover(function () {
+//    clearInterval(tabCycle);
+//}, function () {
+//    tabCycle = setInterval(tabChange, 5000);
+//});
+
+//// Tab Cycle function
+//var tabCycle = setInterval(tabChange, 5000)
+
+$(function () {
+    $('.nav-tabs a').click(function (e) {
+        e.preventDefault();
+        clearInterval(tabCycle);
+        $(this).tab('show')
+        tabCycle = setInterval(tabChange, 5000);
+    });
+});
+
+
 
