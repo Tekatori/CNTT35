3c43f679d2acb4cc89ae7bec1076187604685e16
LocBui <LocBui@DESKTOP-IJO1K2O>
2022-12-26 00:28:39 +0700

    check validate email, tel

--- CNTT35/Content/js/site.js
+++ CNTT35/Content/js/site.js
@@ -169,62 +169,25 @@ $(document).ready(function () {
 });
 
 
+const validateEmail = (email) => {
+    return email.match(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
 
-// Javascript validate form
+const validate = () => {
+    const $result = $('#result');
+    const email = $('#email').val();
+    $result.text('');
 
-const btnRegister = document.getElementById('btn-register');
-
-btnRegister.addEventListener('click', function () {
-    let isValid = checkValidate();
-
-    if (isValid) {
-        alert('Gửi đăng ký thành công');
-    }
-});
-
-// Truy cập vào các ô input
-const usernameEle = document.getElementById('username');
-const emailEle = document.getElementById('email');
-const phoneEle = document.getElementById('phone');
-
-// Validate dữ liệu trong các ô input và highlight
-function checkValidate() {
-    let usernameValue = usernameEle.value;
-    let emailValue = emailEle.value;
-    let phoneValue = phoneEle.value;
-
-    let isCheck = true;
-
-    // Kiểm tra trường username
-    if (usernameValue == '') {
-        setError(usernameEle, 'Tên không được để trống');
-        isCheck = false;
+    if (validateEmail(email)) {
+        $result.text(email + ' đúng định dạng');
+        $result.css('color', 'green');
     } else {
-        setSuccess(usernameEle);
+        $result.text(email + ' không đúng định dạng');
+        $result.css('color', 'red');
     }
-
-    // Kiểm tra trường email
-    if (emailValue == '') {
-        setError(emailEle, 'Email không được để trống');
-        isCheck = false;
-    } else if (!isEmail(emailValue)) {
-        setError(emailEle, 'Email không đúng định dạng');
-        isCheck = false;
-    } else {
-        setSuccess(emailEle);
-    }
-
-    // Kiểm tra trường phone
-    if (phoneValue == '') {
-        setError(phoneEle, 'Số điện thoại không được để trống');
-        isCheck = false;
-    } else if (!isPhone(phoneValue)) {
-        setError(phoneEle, 'Số điện thoại không đúng định dạng');
-        isCheck = false;
-    } else {
-        setSuccess(phoneEle);
-    }
-
-    return isCheck;
+    return false;
 }
 
+$('#email').on('input', validate);
